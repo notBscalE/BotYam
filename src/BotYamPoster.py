a314903f06553cb5a8240e2f9ce39f8b57be14a6
Lucy Joshua <43093984+notBscalE@users.noreply.github.com>
2023-01-31 10:57:48 +0200

    Tilt debugging

--- src/BotYamPoster.py
+++ src/BotYamPoster.py
@@ -70,7 +70,10 @@ class BotYamPoster(tweepy.StreamingClient):
                 reply_bank['special_gags']['misadot']['keywords'],
                 reply_bank['special_gags']['misadot']['reply'],
                 postcounter)
-        
+
+        if (postcounter == 0):
+            print("Post counter for this tweet: 0! Posting tilt.")
+
         if any(tilter in tweet.data['text'] for tilter in reply_bank['special_gags']['tilt']['keywords']) or (postcounter == 0 and "@FromBotYam" in tweet.data['text']):
             postcounter = post_reply(
                 conn,
