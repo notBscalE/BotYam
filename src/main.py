8b728793ceb150f5895a7e652b988200c5f5cb19
Lucy Joshua <43093984+notBscalE@users.noreply.github.com>
2023-03-22 14:35:11 +0200

    Time stamps

--- src/main.py
+++ src/main.py
@@ -1,18 +1,19 @@
 import sys
 import tweepy
+import time
 from BotYamPoster import BotYamPoster
 from BotYamPoster import init_streamobject
 from connector import Connector
 
 def main():
-    print("BOT-YAM - VERSION 3.0.2 >>>>")
+    print(time.time() + ": " + "BOT-YAM - VERSION 3.0.2 >>>>")
     conn = Connector()
     stream = init_streamobject(conn)
-    print("Adding stream rules...")
+    print(time.time() + ": " + "Adding stream rules...")
     stream.add_rules(tweepy.StreamRule(conn.get_victims()['stream_filter']))
     stream.add_rules(tweepy.StreamRule("@FromBotYam"))
     # Start listening for tweets
-    print("Starting Twitter stream!")
+    print(time.time() + ": " + "Starting Twitter stream!")
     stream.filter(expansions="author_id")
 
 if __name__ == "__main__":
